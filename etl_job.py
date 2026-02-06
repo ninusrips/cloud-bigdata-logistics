eeae09e43410d0ddc3f260ea33af07a46b1166b2
Ninusri ps <ninusrips@gmail.com>
2026-02-06 10:21:40 +0530

    Update etl_job.py

--- etl_job.py
+++ etl_job.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("LogisticsETL").getOrCreate()
-df = spark.read.csv("s3a://your-bucket-name/raw/orders.csv", header=True)
+df = spark.read.csv("s3a://ninusri/raw/orders.csv", header=True)
 clean = df.dropna().dropDuplicates()
-clean.write.mode("overwrite").parquet("s3a://your-bucket-name/processed/orders/")
+clean.write.mode("overwrite").parquet("s3a://ninusri/processed/orders/")
 print("ETL completed")
